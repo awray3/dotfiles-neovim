7cdba1bf1ed249caadc8b1d94451a520d8d92713
Andrew Wray <andrewwray2718@gmail.com>
2022-04-15 19:18:39 -0700

    Fix keymappings

--- .config/nvim/lua/lsp/common.lua
+++ .config/nvim/lua/lsp/common.lua
@@ -1,8 +1,4 @@
 -- Sets up LSP Config Plugin
-local common = require("common")
---local lspformat = require("lsp-format")
-
---lspformat.setup {}
 
 local function diagnostic_config()
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
@@ -28,20 +24,21 @@ M.on_attach = function(client)
         --lspformat.on_attach(client)
 
 	-- Mappings.
-	local opts = { noremap = true, silent = true }
+	local opts = { silent = true }
 
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	common.buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	common.buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	common.buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	common.buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	common.buf_set_keymap("n", "ga", "<cmd>Telescope lsp_code_actions<CR>", opts)
-	common.buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-	common.buf_set_keymap("n", "gl", "<cmd>Telescope lsp_document_diagnostics<CR>", opts)
-	common.buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-	common.buf_set_keymap("n", "gs", "<cmd>Telescope lsp_document_symbols<CR>", opts)
-	common.buf_set_keymap("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-	common.buf_set_keymap("n", "gw", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
+        
+	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.keymap.set("n", "ga", "<cmd>Telescope lsp_code_actions<CR>", opts)
+	vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+	vim.keymap.set("n", "gl", "<cmd>Telescope lsp_document_diagnostics<CR>", opts)
+	vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+	vim.keymap.set("n", "gs", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+	vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+	vim.keymap.set("n", "gw", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
 end
 
 vim.cmd("set completeopt=menu,preview,noselect")
