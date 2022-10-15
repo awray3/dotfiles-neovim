d41f44649d95b2ec1057a9787a38e2d47540be3e
Andrew Wray <andrewwray2718@gmail.com>
2022-10-15 16:30:03 -0700

    Fix nvim-cmp-lsp configuration

--- .config/nvim/lua/aw/lsp/common.lua
+++ .config/nvim/lua/aw/lsp/common.lua
@@ -64,7 +64,9 @@ end
 M.on_attach = on_attach
 
 -- completion capabilities
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+--local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 M.capabilities = capabilities
 
 return M
