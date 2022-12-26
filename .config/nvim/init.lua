80a6523b1aec87192128213491c8bd19ccca4141
Andrew Wray <andrewwray2718@gmail.com>
2022-12-25 16:03:21 -0800

    Formatting and add bufdelete.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -478,16 +478,12 @@ require('packer').startup {
         use {
             'famiu/bufdelete.nvim',
             config = function()
-                vim.keymap.set('n',
-                    '<Leader>q',
-                    function()
-                        require('bufdelete').bufdelete(0, true)
-                end,
-            {
-                noremap = true,
-                silent = true,
-            }
-                )
+                vim.keymap.set('n', '<Leader>q', function()
+                    require('bufdelete').bufdelete(0, true)
+                end, {
+                    noremap = true,
+                    silent = true,
+                })
             end,
         }
 
--- .config/nvim/lua/aw/lsp/init.lua
+++ .config/nvim/lua/aw/lsp/init.lua
@@ -8,8 +8,8 @@
 
 -- general diagnostic configuration
 vim.diagnostic.config {
-    virtual_text = false,
-    update_in_insert = false,
+  virtual_text = false,
+  update_in_insert = false,
 }
 
 -- See this module for all of the default lsp configuration.
@@ -28,49 +28,57 @@ local servers = { 'clangd', 'rust_analyzer', 'pyright', 'sumneko_lua' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
-    ensure_installed = servers,
+  ensure_installed = servers,
 }
 
 require 'aw.lsp.null-ls'
 
 for _, lsp in ipairs(servers) do
-    -- sumneko lua handled separately
-    if lsp ~= 'sumneko_lua' then
-        require('lspconfig')[lsp].setup {
-            on_attach = common_lsp_config.on_attach,
-            capabilities = common_lsp_config.capabilities,
-        }
-    elseif lsp == 'sumneko_lua' then
-        -- Make runtime files discoverable to the server
-        local runtime_path = vim.split(package.path, ';', {})
-        table.insert(runtime_path, 'lua/?.lua')
-        table.insert(runtime_path, 'lua/?/init.lua')
+  -- sumneko lua handled separately
+  local lspconfig = require("lspconfig")
+  
+  if lsp ~= 'sumneko_lua' then
+    lspconfig[lsp].setup {
+      on_attach = common_lsp_config.on_attach,
+      capabilities = common_lsp_config.capabilities,
+    }
+  elseif lsp == 'sumneko_lua' then
+    -- Make runtime files discoverable to the server
+    local runtime_path = vim.split(package.path, ';', {})
+    table.insert(runtime_path, 'lua/?.lua')
+    table.insert(runtime_path, '/lua/?/init.lua')
 
-        local lua_ls_on_attach = function (client, bufnr)
-          common_lsp_config.on_attach(client, bufnr)
-          client.server_capabilities.document_formatting = false
-          client.server_capabilities.document_range_formatting = false
-        end
-
-        require('lspconfig').sumneko_lua.setup {
-            on_attach = lua_ls_on_attach,
-            capabilities = common_lsp_config.capabilities,
-            settings = {
-                Lua = {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-                        version = 'LuaJIT',
-                        -- Setup your lua path
-                        path = runtime_path,
-                    },
-                    diagnostics = {
-                        globals = { 'vim', 'bufnr' },
-                    },
-                    telemetry = { enable = false },
-                },
-            },
-        }
-    end
+    lspconfig['sumneko_lua'].setup {
+      on_attach = common_lsp_config.on_attach,
+      capabilities = common_lsp_config.capabilities,
+      root_dir = function()
+        return "~/.config/nvim"
+      end,
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+          },
+          diagnostics = {
+            globals = { 'vim', 'bufnr' },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
+          completion = {
+            enable = true,
+          },
+          format = {
+            enable = false,
+          },
+          telemetry = { enable = false },
+        },
+      },
+    }
+  end
 end
 
 -- Completion is setup here
@@ -79,42 +87,42 @@ local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
-    mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete({}),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
